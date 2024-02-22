676e9d58e0e430dd35d11e5cba7c563f8d668c1c
Muhammet Mustafa Çor <mustafa.c@voyantsmart.com>
2024-02-22 14:11:57 +0300

    webhook signature logic added

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { createHash } = require("crypto");
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
+function hashBody(body, secret) {
+  return createHash("sha256")
+    .update(body)
+    .update(createHash("sha256").update(secret, "utf8").digest("hex"))
+    .digest("hex");
+}
+
 app.post("/webhook", (req, res) => {
-  console.log(req.body);
-  res.send("OK");
+  const signature = req.headers["x-signature"];
+  const secretKey = process.env.WEBHOOK_SECRET;
+  if (signature !== hashBody(JSON.stringify(req.body), secretKey)) {
+    res.status(401).send("Invalid signature");
+    return;
+  } else {
+    console.log(req.body);
+    res.send("OK");
+  }
 });
 
 app.listen(process.env.PORT, () => {
